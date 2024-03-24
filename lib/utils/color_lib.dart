56ff9edbd33c90e26d6f16d17f97a39a98e87d35
Codebox124 <rajcodes733@gmail.com>
2024-03-24 17:52:33 +0100

    updated

--- lib/utils/color_lib.dart
+++ lib/utils/color_lib.dart
@@ -4,4 +4,7 @@ class ColorLib {
   static const kPrimary = Color.fromARGB(255, 47, 103, 246);
   static const ktextBlack = Color.fromARGB(255, 17, 24, 60);
   static const koutline = Color(0xffD7D9E4);
+    static const ktextgreen = Color(0xff098C26);
+ 
+
 }
--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -79,13 +79,156 @@ class HomePage extends StatelessWidget {
                   ),
                 ],
               ),
-              SizedBox(
+              const SizedBox(
                 height: 30,
               ),
               Container(
-                height: 200,
+                height: 128,
                 child: Placeholder(),
-              )
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Market Movers",
+                    style: TextStyle(
+                      color: ColorLib.ktextBlack,
+                      fontSize: 18,
+                    ),
+                  ),
+                  Text(
+                    "More",
+                    style: TextStyle(
+                      color: ColorLib.kPrimary,
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Container(
+                height: 190,
+                child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 0.5,
+                              blurRadius: 8,
+                              offset:
+                                  Offset(0, 3), // changes position of shadow
+                            ),
+                          ],
+                        ),
+                        width: 156,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  const Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "BTC/USD",
+                                        style: TextStyle(
+                                          color: ColorLib.ktextBlack,
+                                          fontWeight: FontWeight.w500,
+                                          fontSize: 20,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        height: 5,
+                                      ),
+                                      Text(
+                                        "30,113.80",
+                                        style: TextStyle(
+                                          color: ColorLib.ktextBlack,
+                                          fontSize: 16,
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                  Image.asset(
+                                    "assets/img/Bitcoin_perspective_matte.png",
+                                  )
+                                ],
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              const Text(
+                                "+2.76%",
+                                style: TextStyle(
+                                  color: ColorLib.ktextgreen,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 15,
+                              ),
+                              Image.asset(
+                                "assets/img/Vector (4).png",
+                              ),
+                              const SizedBox(
+                                height: 15,
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text("24H Vol."),
+                                  Text("394 897 432,26")
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                    separatorBuilder: (context, index) {
+                      return const SizedBox(
+                        width: 20,
+                      );
+                    },
+                    itemCount: 5),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Portfolio",
+                    style: TextStyle(
+                      color: ColorLib.ktextBlack,
+                      fontSize: 18,
+                    ),
+                  ),
+                  Text(
+                    "More",
+                    style: TextStyle(
+                      color: ColorLib.kPrimary,
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
